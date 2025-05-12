e0b73a12f681ec260bac67f78fb809cfa1ba3e06
sinzem <111191960+sinzem@users.noreply.github.com>
2025-05-12 23:33:23 +0300

    Add files via upload

---
+++ cases/basket/Attraction_particles/script.js
@@ -0,0 +1,124 @@
+// https://www.youtube.com/@ThreePixDroid_RU
+
+(() => {
+
+    const config = { /* (настройки) */
+        dotMinRad: 6,
+        dotMaxRad: 20,
+        sphereRad: 300, /* (радиус отталкивания точек) */
+        bigDotRad: 35, /* (для начальной точки) */
+        mouseSize: 120, /* (расстояние отталкивания точек от курсора) */
+        massFactor: 0.002,
+        defColor: "rgba(250, 10, 30, 0.9)",
+        smooth: 0.65, /* (для торможения, уменьшения скорости точек) */
+    }
+
+    const TWO_PI = 2 * Math.PI;
+
+    const canvas = document.querySelector("#canvas");
+    const ctx = canvas.getContext("2d");
+
+    let w, h, mouse, dots;
+
+    class Dot { /* (для создания точек) */
+        constructor(r) {
+            this.pos = {x: mouse.x , y: mouse.y};
+            this.vel = {x: 0, y: 0}; /* (скорость) */
+            this.rad = r || random(config.dotMinRad, config.dotMaxRad); /* (радиус) */this.mass = this.rad * config.massFactor;
+            this.color = config.defColor;
+        }
+
+        draw(x, y) { /* (отрисовка круга и ободка) */
+            this.pos.x = x || this.pos.x + this.vel.x;
+            this.pos.y = y || this.pos.y + this.vel.y;
+            createCircle(this.pos.x, this.pos.y, this.rad, true, this.color);
+            createCircle(this.pos.x, this.pos.y, this.rad, false, config.defColor);
+        }
+    }
+
+    function updateDots() { /* (притяжение точек) */
+        for (let i = 1; i < dots.length; i++) {
+            let acc = {x: 0, y: 0};
+
+            for (let j = 0; j < dots.length; j++) {
+                if (i == j) continue;
+
+                let [a, b] = [dots[i], dots[j]];
+
+                let delta = {x: b.pos.x - a.pos.x, y: b.pos.y - a. pos.y};
+                let dist = Math.sqrt(delta.x * delta.x + delta.y * delta.y) || 1;
+
+                let force = (dist - config.sphereRad) / dist * b.mass;
+                
+                if (j == 0) {
+                    let alpha = config.mouseSize / dist;
+                    a.color = `rgba(250, 10, 30, ${alpha})`; /* (прозрачность точек взависимости от удаления от курсора) */
+
+                    dist < config.mouseSize 
+                        ? force = (dist - config.mouseSize) * b.mass 
+                        : force = a.mass;
+                        // : force = 0; (точки не преследуют курсор)
+                        // : force = 1; (точки очень быстро следуют за курсором)
+                }
+
+                acc.x += delta.x * force;
+                acc.y += delta.y * force;
+            }
+
+            dots[i].vel.x = dots[i].vel.x * config.smooth + acc.x * dots[i].mass;
+            dots[i].vel.y = dots[i].vel.y * config.smooth + acc.y * dots[i].mass;
+        }
+
+        dots.map(e => e == dots[0] ? e.draw(mouse.x, mouse.y) : e.draw());
+    }
+
+    function createCircle(x, y, rad, fill, color) {
+        ctx.fillStyle = ctx.strokeStyle = color;
+        ctx.beginPath();
+        ctx.arc(x, y, rad, 0, TWO_PI);
+        ctx.closePath();
+        fill ? ctx.fill() : ctx.stroke();
+    }
+
+    function random(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+
+    function init() {
+        w = canvas.width = innerWidth;
+        h = canvas.height = innerHeight;
+
+        mouse = {x: w / 2, y: h / 2, down: false};
+        dots = [];
+        
+        dots.push(new Dot(config.bigDotRad))
+    }
+
+    function loop() {
+        ctx.clearRect(0, 0, w, h);
+
+        if (mouse.down) {
+            dots.push(new Dot());
+            console.log(dots.length);
+        }
+
+        updateDots();
+
+        window.requestAnimationFrame(loop);
+    }
+
+    init();
+    loop();
+
+    function setPos({layerX, layerY}) {
+        [mouse.x, mouse.y] = [layerX, layerY];
+    }
+
+    function isDown() {
+        mouse.down = !mouse.down;
+    }
+
+    canvas.addEventListener("mousemove", setPos);
+    window.addEventListener("mousedown", isDown);
+    window.addEventListener("mouseup", isDown);
+})()
