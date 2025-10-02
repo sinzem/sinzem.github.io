58c8136f3ca2ffa264586b2866296f924a9a4d50
sinzem <111191960+sinzem@users.noreply.github.com>
2025-10-02 08:06:27 +0300

    Add files via upload

--- cases/basket/YD_test_task/scripts/main.js
+++ cases/basket/YD_test_task/scripts/main.js
@@ -70,17 +70,42 @@ countriesBtnBlock.addEventListener("click", () => {
 })
 
 // calendar=======================================
+const orderButton = document.querySelector(".calendar_order");
 const monthAndYear = document.querySelector(".calendar_months");
 const daysContainer = document.querySelector(".calendar_days");
 const daysNamesContainer = document.querySelector(".calendar_day__names");
 const prevBtn = document.querySelector(".calendar_prev");
 const nextBtn = document.querySelector(".calendar_next");
 
+let selectedDays = [];
+
+daysContainer.addEventListener("click", (e) => {
+    if (e.target.classList.contains("calendar_day")) {
+        const checked = (monthAndYear.textContent).split(' ');
+        const index = monthNames.findIndex(i => checked[0] === i)
+        const checkedDate = String(new Date(+checked[1], index, +e.target.textContent));
+        if (!e.target.classList.contains("active")) {
+            e.target.classList.add("active");
+            selectedDays.push(checkedDate);
+        } else {
+            e.target.classList.remove("active");
+            selectedDays.forEach((date, i) => date === checkedDate ? selectedDays.splice(i, 1) : null);
+        }
+    }
+})
+
+orderButton.addEventListener("click", () => {
+    console.log(selectedDays);
+    selectedDays = [];
+    renderCalendar();
+})
+
 const monthNames = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
 
 const dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 let currentDate = new Date();
+const stringToday = String(new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate()));
 
 const renderDayNames = () => {
     daysNamesContainer.innerHTML = dayNames.map(day => `<div>${day}</div>`).join('');
@@ -95,21 +120,27 @@ const renderCalendar = () => {
     const firstDay = ((new Date(year, month).getDay() + 7) % 7);
     const daysInMonth = 32 - new Date(year, month, 32).getDate();
     const daysInPrevMonth = 32 - new Date(year, month - 1, 32).getDate();
-    const daysInNextMonth = 32 - new Date(year, month + 1, 32).getDate();
-    console.log(daysInPrevMonth, daysInNextMonth);
+    const prevDifferense = daysInPrevMonth - firstDay;
+    const firstDayNext = (7 - ((new Date(year, month + 1).getDay() + 7) % 7));
 
     daysContainer.innerHTML = '';
 
     for (let i = 0; i < firstDay; i++) {
-        daysContainer.innerHTML += `<div class="clendar_days__hidden"></div>`;
+        daysContainer.innerHTML += `<div class="calendar_day__hidden">${prevDifferense + i + 1}</div>`;
     }
 
     for (let day = 1; day <= daysInMonth; day++) {
-        const isToday = day === new Date().getDate() &&
-                        month === new Date().getMonth() &&
-                        year === new Date().getFullYear();
+        const stringDate = String(new Date(year, month, day));
+        const isToday = stringDate === stringToday;
+        const isChecked = selectedDays.includes(stringDate);
 
-        daysContainer.innerHTML += `<div class="${isToday ? "calendar_today" : ''}">${day}</div>`
+        daysContainer.innerHTML += `<div class="${isToday ? "calendar_day calendar_day__today" : 'calendar_day'} ${isChecked ? "active" : ''}">${day}</div>`
+    }
+
+    if (firstDayNext > 0 && firstDayNext < 7) {
+       for (let i = 0; i < firstDayNext; i++) {
+            daysContainer.innerHTML += `<div class="calendar_day__hidden">${i + 1}</div>`;
+        } 
     }
 }
 
@@ -124,9 +155,104 @@ nextBtn.addEventListener("click", () => changeMonth(1));
 renderDayNames();
 renderCalendar();
 
+// drivers_list================================
+
+const drivers = document.querySelectorAll(".drivers_card");
+const driversBtn = document.querySelector(".drivers_top button");
+driversBtn.children[0].textContent = `${drivers.length}`;
+let isAllDrivers = false;
+
+drivers.forEach((driver, i) => i > 4 ? driver.classList.add("hidden") : null);
+
+driversBtn.addEventListener("click", () => {
+    if (!isAllDrivers) {
+        drivers.forEach(driver => {
+            isAllDrivers = true;
+            driver.classList.remove("hidden");
+        });
+    } else {
+         drivers.forEach((driver, i) => {
+            isAllDrivers = false;
+            i > 4 ? driver.classList.add("hidden") : null;
+        });
+    }
+})
+
+// reviews=====================================
+
+class Card {
+    constructor({id, photo, name, linkUrl, linkText, date, text, rating}, parentSelector) {
+        this.id = id;
+        this.photo = photo;
+        this.name = name;
+        this.linkUrl = linkUrl;
+        this.linkText = linkText;
+        this.date = date;
+        this.text = text;
+        this.rating = rating;
+        this.parent = document.querySelector(parentSelector);
+    }
+
+    ratingStars() {
+        let stars = '';
+        for (let i = 0; i < this.rating; i++) {
+            stars += `<img src="./assets/img/icon/png/icon_testimonials_star_1.png" alt="star">`;
+        }
+        return stars;
+    }
+ 
+    render() {
+        const element = document.createElement("div"); 
+        element.classList.add("reviews_card");
+
+        element.innerHTML = ` 
+        <div class="reviews_card__top">
+            <div class="reviews_card__img">
+                <img src="${this.photo}" alt="client's photo">
+            </div>
+            <div class="reviews_card__info">
+                <h2>${this.name}</h2>
+                <a href="${this.linkUrl}">${this.linkText}</a>
+                <h4>${this.date}</h4>
+            </div>
+        </div>
+        <div class="reviews_text reviews_card__description">${this.text}</div>
+        <div class="reviews_card__stars">${this.ratingStars()}</div>
+        `;
+
+        this.parent.append(element); 
+    }
+}
+
+const reviewLine = document.querySelector(".reviews_line");
+const reviewCards = document.getElementsByClassName("reviews_card");
+const reviewPagination = document.querySelector(".revievs_pagination");
+const reviewDots = document.getElementsByClassName("revievs_pagination__dot");
+let activeDot = 0;
+
+reviewsData.forEach(obj => new Card(obj, ".reviews_line").render());
+let lineGap = parseInt(window.getComputedStyle(reviewLine).gap);
+let cardWidth = parseInt(window.getComputedStyle(reviewCards[0]).width);
+
+for (let i = 0; i < reviewCards.length; i++) {
+    reviewPagination.innerHTML += `<div class="revievs_pagination__dot"></div>`;
+}
+
+const reviewDotsArr = Array.from(reviewDots);
+reviewDotsArr.forEach((dot, i) => {
+    if (i === activeDot) dot.classList.add("active");
+    dot.addEventListener("click", () => {
+        if (i !== activeDot) {
+            activeDot = i;
+            reviewLine.style.transform = `translateX(-${activeDot * (cardWidth + lineGap)}px)`;
+            reviewDotsArr.forEach((pag, j) => 
+                j !== i ? pag.classList.remove("active") : pag.classList.add("active")
+            )
+        }
+    })
+});
+
 // services====================================
-
-
 
 function checkEmail(email) {
     const emailPrepared = email.trim();
@@ -178,3 +304,6 @@ function throttle(func, ms) {
 
     return wrapper;
 }
+
+
+
