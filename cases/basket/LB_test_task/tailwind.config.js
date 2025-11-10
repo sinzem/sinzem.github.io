23fb04949018138bfa125ad5af7e923ea962c0f1
sinzem <111191960+sinzem@users.noreply.github.com>
2025-11-10 09:51:46 +0200

    Add files via upload

---
+++ cases/basket/LB_test_task/tailwind.config.js
@@ -0,0 +1,33 @@
+tailwind.config = {
+    content: ["./*.html"],
+    plugins: [],
+    theme: {
+        extend: {
+            colors: {
+                r_black: '#000000',
+                r_white: '#FFFFFF',
+                r_gray: '#A8A8A8',
+                r_gray_text: '#ABABAB',
+                r_d_gray: '#7E7C7C',
+                r_gray_bg: '#F2F2F2',
+                r_red: '#FF6161',
+                r_orange: '#FD6D20',
+                r_green: '#6CC471',
+                r_l_green: '#7CFF4C',
+            },
+            backgroundImage: {
+                gr_orange: 'linear-gradient(to right, #FF7E2D, #F64200)',
+            }
+        },
+        container: {
+            center: true, 
+            screens: {
+                sm: '640px',
+                md: '768px',
+                lg: '1024px',
+                xl: '1280px',
+                "2xl": '1440px',
+            },
+        },
+    },  
+}
